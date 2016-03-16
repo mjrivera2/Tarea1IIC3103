ebe99824d046a7d86fdce8871c99977f35dca1e9
mjrivera2 <mjrivera2@uc.cl>
2016-03-15 22:45:23 -0300

    a

--- src/AppHarbour.Web/Controllers/HomeController.cs
+++ src/AppHarbour.Web/Controllers/HomeController.cs
@@ -1,9 +1,4 @@
 ﻿using System.Web.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Mvc;
 
 namespace AppHarbour.Web.Controllers
 {
