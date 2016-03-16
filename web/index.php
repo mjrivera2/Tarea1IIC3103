9162269fdf53164662d2030fd02b25448a76c4f6
mjrivera2 <mjrivera2@uc.cl>
2016-03-16 17:11:18 -0300

    si?

--- web/index.php
+++ web/index.php
@@ -22,10 +22,29 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
-$app->get('/validarFirma', function() use($app) {
+$app->post('/validarFirma', function() use($app) {
   ?>
   hola
 <?php
+});
+
+$app->get('/status', function() use($app) {
+  
+  $response = new Response();
+
+    // Check if the insertion was successful
+    if ($status->success() == true) {
+
+        // Change the HTTP status
+        $response->setStatusCode(201, "Created");
+
+        $robot->id = $status->getModel()->id;
+
+        $response->setJsonContent(
+            array(
+                'status' => 'OK',
+            )
+        );
 });
 
 $app->run();
