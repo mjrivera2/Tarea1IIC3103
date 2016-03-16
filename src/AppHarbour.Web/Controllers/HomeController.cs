b5d7067b4f87b5bca23ed0a93779b437b962df27
mjrivera2 <mjrivera2@uc.cl>
2016-03-15 22:43:50 -0300

    a

---
+++ src/AppHarbour.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace AppHarbour.Web.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return RedirectToAction("ViewController", 
+                        "Get");
+		}
+	}  
+}
+
+
