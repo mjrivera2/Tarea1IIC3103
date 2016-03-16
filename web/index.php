106926e389732dd0a15824feab544714289da1a0
mjrivera2 <mjrivera2@uc.cl>
2016-03-16 18:01:21 -0300

    a

--- web/index.php
+++ web/index.php
@@ -22,7 +22,7 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
-$app->post('/validarFirma/{mensaje}', function($mensaje) use($app) {
+$app->post('/validarFirma?{firma}={mensaje}', function($mensaje) use($app) {
 	//$data = hash($firma, $mensaje);
 	header('Content-Type: application/json');
 	return $mensaje;
